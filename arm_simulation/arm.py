7ccf0b64d6028f005574cfef28177c258ee70e4d
Alessio-17 <56605444+Alessio-17@users.noreply.github.com>
2021-11-06 11:33:50 +0100

    Using cartesian coordinates

--- arm_simulation/arm.py
+++ arm_simulation/arm.py
@@ -3,10 +3,11 @@ from roboticstoolbox import DHRobot, RevoluteMDH, ERobot, ELink, ETS
 from spatialmath import SE3
 from roboticstoolbox.tools.trajectory import tpoly,lspb
 from math import pi as pi
+from spatialmath.base import transl, trotx
 import numpy as np
 
 # DH parameters
-a = [0, 0, 1.2, 0, 0.8, 0]
+a = [0, 0, 0.5, 0, 0.4, 0]
 d = [0, 0, 0, 0, 0, 0]
 alpha = [0, pi/2, 0, pi/2, pi/2, pi/2]
 
@@ -18,14 +19,22 @@ robot = DHRobot([RevoluteMDH(d[0], a[0], alpha[0]),
     RevoluteMDH(d[5], a[5], alpha[5])],
     name='6R')
 
-time = np.array(range(1,100))
+time = np.array(range(1,100, 5))
 
-T1 = robot.fkine([pi/2, pi/3, -pi/2, 0, 0, 0])
+#T1 = robot.fkine([pi/2, pi/3, -pi/2, 0, 0, 0])
 T2 = robot.fkine([-pi/2, pi/6, -pi/3, pi/4, 3, 3])
 
+'''
+transl(x, y, z)
+'''
+P1 = SE3(transl(0.5, 0.1, 0.3) * trotx(pi))
+P2 = SE3(transl(0.1, -0.5, -0.3))
+print(SE3(P2))
+print(T2)
+
 #[q_des_1, qd_des_1, qdd_des_1] = robot.jtraj(T1=T1, T2=T2, t=time)
-trajectory = robot.jtraj(T1=T1, T2=T2, t=time)
-print(robot.jtraj(T1=T1, T2=T2, t=time))
+trajectory = robot.jtraj(T1=P1, T2=P2, t=time)
+print(robot.jtraj(T1=P1, T2=P2, t=time))
 
 #figure("Name", "animation")
 
